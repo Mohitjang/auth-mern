675fa17f30ef16d399a0509f60c20bdad4adcec2
Mohit Jangid <mjangidjj12@gmail.com>
2025-06-21 06:39:51 +0530

    deploy fix1: deploment error fix!

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
+// Correct: Tailwind works via PostCSS, no need to import it here
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
 });
