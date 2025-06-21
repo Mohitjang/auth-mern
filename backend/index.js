8e756ca8a605ed56dd23408a422e6df0ecfe4071
Mohit Jangid <mjangidjj12@gmail.com>
2025-06-21 05:59:58 +0530

    Deployment related code added

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,8 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-// import { connectDB } from "../db/connectDB.js";
+import path from "path";
+
 import { connectDB } from "./db/connectDB.js";
 import authRoutes from "./routes/auth.routes.js";
 import cors from "cors";
@@ -11,18 +12,22 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
 app.use(express.json());
 app.use(cookieParser());
 
-app.get("/", (req, res) => {
-  res.send("hello world !");
-});
-
 // redirecting to auth routes
 app.use("/api/auth", authRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   connectDB();
